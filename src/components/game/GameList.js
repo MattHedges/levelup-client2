4831e35ddf0b2e35efeec48b87cd35e40fa786a8
Matthew Hedges <matthedges35@gmail.com>
2023-06-12 18:22:51 -0500

    game folder complete

--- src/components/game/GameList.js
+++ src/components/game/GameList.js
@@ -1,23 +1,60 @@
 import React, { useEffect, useState } from "react"
-import { getGames } from "../../managers/GameManager.js"
+import { useNavigate } from "react-router-dom"
+import { deleteGame, getGames } from "../../managers/GameManager.js"
+import "./game.css"
 
 export const GameList = (props) => {
-    const [ games, setGames ] = useState([])
+    const [ refresh, setRefresh ] = useState(true)
+    const [ games, setGames ] = useState([
+        {id: 0,
+        name: "",
+        description: "",
+        game_type: {},
+        min_player: 0,
+        max_player: 0,
+        gamer: {}
+        }
+    ]
+    )
+    const navigate = useNavigate()
+
+    function refreshPage() {
+        window.location.reload(false)
+    }
 
     useEffect(() => {
         getGames().then(data => setGames(data))
-    }, [])
+    }, [,refresh])
+
+    const handleClick = (id) => {
+        deleteGame(id).then(refreshPage)
+    }   
 
     return (
         <article className="games">
+            <button className="btn btn-2 icon-create"
+                onClick={() => {
+                    navigate({ pathname: "new" })
+                }}
+            >Register New Game</button>
             {
                 games.map(game => {
                     return <section key={`game--${game.id}`} className="game">
-                        <div className="game__title">{game.name}:</div>
-                        <div className="game__type">Game Type: {game.game_type}</div>
-                        <div className="game__description">Game Description: {game.description}</div>
-                        <div className="game__players">{game.min_players} Minimum Players Needed</div>
-                        <div className="game__players">{game.max_players} Maximum Players Needed</div>
+                        <div className="game__title">{game.name}</div>
+                        <div className="game__players">{game.min_player} players needed</div>
+                        <div className="game__skillLevel">Game Type: {game.game_type.label}</div>
+                        <div className="game__footer">
+                            <button
+                                onClick={() => {
+                                    navigate({ pathname: `edit/${game.id}`})
+                                }}>Edit</button>
+                        </div>
+                        <div className="game__footer">
+                            <button
+                                onClick={() => {
+                                    handleClick(game.id)
+                                }}>Delete</button>
+                        </div>
                     </section>
                 })
             }
--- src/views/ApplicationViews.js
+++ src/views/ApplicationViews.js
@@ -1,8 +1,13 @@
 import { Route, Routes } from "react-router-dom"
 import { Login } from "../components/auth/Login"
 import { Register } from "../components/auth/Register"
-import { Authorized } from "./Authorized"
+import { EventForm } from "../components/event/EventForm"
+import { EventList } from "../components/event/EventList"
+import { UpdateEvent } from "../components/event/UpdateEvent"
+import { GameForm } from "../components/game/GameForm"
 import { GameList } from "../components/game/GameList"
+import { UpdateGame } from "../components/game/UpdateGame"
+import { Authorized } from "./Authorized"
 
 
 export const ApplicationViews = () => {
@@ -12,6 +17,18 @@ export const ApplicationViews = () => {
             <Route path="/register" element={<Register />} />
             <Route element={<Authorized />}>
                 <Route path="/" element={<GameList />} />
+                <Route path="games" >
+                    <Route index element={<GameList />} />
+                    <Route path="new" element={<GameForm />} />
+                    <Route path="edit/:gameId" element={<UpdateGame />} />
+                </Route>
+                <Route path="events" >
+                    <Route index element={<EventList />} />
+                    <Route path="new" element={<EventForm />} />
+                    <Route path="edit/:eventId" element={<UpdateEvent />} />
+                </Route>
+                <Route path="gamers" element={<GameList />} />
+                
             </Route>
         </Routes>
     </>
